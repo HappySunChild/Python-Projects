2737b18207a7b78d4b61620b41bf96f6babc2f6d
HappySunChild <90355743+HappySunChild@users.noreply.github.com>
2024-03-30 12:09:19 -0400

    woopsies wrong directory

--- Libraries/RoAPI/RoAPI/Endpoints/badges.py
+++
@@ -1,6 +0,0 @@
-# RoAPI badges.py
-
-from ..fetch import fetch, genUrl
-
-def info(badgeId):
-	return fetch(genUrl('badges', f'v1/badges/{badgeId}'))
--- Libraries/RoAPI/RoAPI/Endpoints/economy.py
+++
@@ -1,6 +0,0 @@
-# RoAPI economy.py
-
-from ..fetch import fetch, genUrl
-
-def robux():
-	return fetch(genUrl('economy', 'v1/user/currency'))
